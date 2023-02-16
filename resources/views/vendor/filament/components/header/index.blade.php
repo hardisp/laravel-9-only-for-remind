d931f66c3c570ac06a1792a2784f05a79471f5ab
Hardi <jiksdi12@gmail.com>
2023-02-16 08:40:48 +0800

    feat: customization theme

---
+++ resources/views/vendor/filament/components/header/index.blade.php
@@ -0,0 +1,26 @@
+@props([
+'actions' => null,
+'heading' => 'Jiksdi - Dashbo',
+'subheading' => null
+])
+
+<header {{ $attributes->class(['filament-header space-y-2 items-start justify-between sm:flex sm:space-y-0 sm:space-x-4  sm:rtl:space-x-reverse sm:py-4']) }}>
+    <div>
+        <x-filament::header.heading>
+            <div class="items-center flex ">
+                <div class="mr-2 w-14"><img src="/logo-xs.png" /></div>
+                <h2>{{ $heading }}</h2>
+            </div>
+
+        </x-filament::header.heading>
+
+        @if ($subheading)
+        <x-filament::header.subheading class="mt-1">
+            {{ $subheading }}
+        </x-filament::header.subheading>
+        @endif
+    </div>
+
+
+    <x-filament::pages.actions :actions="$actions" class="shrink-0" />
+</header>
